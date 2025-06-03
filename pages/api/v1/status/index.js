971ec032a00afd72253efb3d125d69e29c1f5f7e
FernandoSchuch <f.schuch@hotmail.com>
2025-06-03 15:43:49 +0000

    finish `status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
-  response.status(200).json({ retorno: "Escreve qualquer coisa aí!" });
+  const updatedAt = new Date().toISOString();
+  const databaseVersion = await database.query({ text: "SHOW server_version" });
+  const databaseMaxConnections = await database.query({
+    text: "SHOW max_connections",
+  });
+  const databaseOpenConnections = await database.query({
+    text: "SELECT count(1)::int as contador FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: parseInt(
+          databaseMaxConnections.rows[0].max_connections,
+        ),
+        open_connections: databaseOpenConnections.rows[0].contador,
+      },
+    },
+  });
 }
 
 export default status;
