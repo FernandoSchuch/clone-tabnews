12837e5b08fa6574771720c4a6c639caa9524791
FernandoSchuch <f.schuch@hotmail.com>
2025-06-05 15:11:40 +0000

    Improving error treatment for database connection

--- infra/database.js
+++ infra/database.js
@@ -8,12 +8,21 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
+  console.log("Credenciais do Postgres", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
   await client.connect();
   var result = null;
   try {
     result = await client.query(queryObject);
   } catch (error) {
     console.error("Database query error:", error);
+    throw error;
   } finally {
     await client.end();
   }
