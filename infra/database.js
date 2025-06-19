749703008662bb6ef234d18875b3f6e1ad2f6f02
FernandoSchuch <f.schuch@hotmail.com>
2025-06-19 00:53:16 +0000

    adicionado log de credenciais do banco

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,13 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  /*console.log("Credenciais do Postgres", {
+  console.log("Credenciais do Postgres", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });*/
+  });
 
   var result = null;
   let client;
